1a4e58506735ef0494e7423342b800ec5ff693cb
raheemadamboev <vividricemaryandmax@gmail.com>
2023-04-23 21:01:20 +0500

    Initial;

---
+++ app/src/main/java/xyz/teamgravity/multilinehinttextfield/MainActivity.kt
@@ -0,0 +1,43 @@
+package xyz.teamgravity.multilinehinttextfield
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import xyz.teamgravity.multilinehinttextfield.ui.theme.MultiLineHintTextFieldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MultiLineHintTextFieldTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MultiLineHintTextFieldTheme {
+        Greeting("Android")
+    }
+}
